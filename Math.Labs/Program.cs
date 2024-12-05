873c2e4eb756c553178d21c4f5a21513405d1573
BlessedDrained <92720096+BlessedDrained@users.noreply.github.com>
2024-12-05 23:17:58 +0500

    Introduce DI to QR lab

--- Math.Labs/Program.cs
+++ Math.Labs/Program.cs
@@ -1,8 +1,48 @@
+using System.Drawing;
+using Math.Code.QrBarcode;
+using ZXing;
+using ZXing.OneD;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using ZXing.Windows.Compatibility;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#pragma warning disable CA1416
+var barcodeWriter = new BarcodeWriter<Bitmap>()
+{
+    Format = BarcodeFormat.CODE_128,
+    Options = new Code128EncodingOptions()
+    {
+        Width = 300,
+        Height = 100,
+        Margin = 20
+    },
+    Renderer = new BitmapRenderer()
+};
+
+builder.Services.AddKeyedSingleton(DiKeyConstants.BarcodeKey, barcodeWriter);
+
+var qrCodeWriter = new BarcodeWriter<Bitmap>()
+{
+    Format = BarcodeFormat.QR_CODE,
+    Options = new QrCodeEncodingOptions()
+    {
+        DisableECI = true,
+        Height = 300,
+        Width = 300,
+        ErrorCorrection = ErrorCorrectionLevel.H
+    },
+    Renderer = new BitmapRenderer()
+};
+
+builder.Services.AddKeyedSingleton(DiKeyConstants.QrKey, qrCodeWriter);
+
+#pragma warning restore CA1416
 
 var app = builder.Build();
 
